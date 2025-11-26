f6d93a11a4eba872787f53f11c148d09b46494ab
ismailwcw <ismailwcw@gmail.com>
2025-11-27 02:36:58 +0600

    used php array foreach and showing multiple card with one singale desing

---
+++ arrey.php
@@ -0,0 +1,160 @@
+<?php
+
+
+
+$brands = [
+    [
+        'brand' => 'Audi',
+        'image' => './asset/img/figma/07.png'
+    ],
+    [
+        'brand' => 'BMW',
+        'image' => './asset/img/figma/b2.jpg.png'
+    ],
+    [
+        'brand' => 'Ford',
+        'image' => './asset/img/figma/b3.jpg.png'
+    ],
+    [
+        'brand' => 'Mercedes-Benz',
+        'image' => './asset/img/figma/b4.jpg.png'
+    ],
+    [
+        'brand' => 'Peugeot',
+        'image' => './asset/img/figma/11.png'
+    ],
+    [
+        'brand' => 'Volkswagen',
+        'image' => './asset/img/figma/12.png'
+    ]
+];
+
+
+$exvehicle = [
+    [
+        "brand" => "Ford",
+        "model" => "Transit",
+        "year" => "2020",
+        "description" => "4.0 D5 PowerPulse Momentum 5dr AW...",
+        "run" => "50,000 miles",
+        "fueltype" => "Diesel",
+        "gearbox" => "Manual",
+        "price" => "22,000",
+        "currency" => "$",
+        "image" => "https://www.motortrend.com/uploads/sites/5/2020/05/2020-ford-transit-connect-02.jpg",
+    ],
+    [
+        "brand" => "Toyota",
+        "model" => "Corolla",
+        "year" => "2021",
+        "description" => "1.8 VVT-i Icon Tech 5dr CVT",
+        "run" => "30,000 miles",
+        "fueltype" => "Hybrid",
+        "gearbox" => "Automatic",
+        "price" => "20,500",
+        "currency" => "$",
+        "image" => "https://cdn.jdpower.com/JDPA_2021%20Toyota%20Corolla%20XSE%20Red%20Front%20Quarter%20View.jpg",
+    ],
+    [
+        "brand" => "Honda",
+        "model" => "Civic",
+        "year" => "2019",
+        "description" => "2.0 i-VTEC EX 5dr CVT",
+        "run" => "40,000 miles",
+        "fueltype" => "Petrol",
+        "gearbox" => "Automatic",
+        "price" => "21,000",
+        "currency" => "$",
+        "image" => "https://tse3.mm.bing.net/th/id/OIP.uUC2_EEkJQWDurUzo5EYQQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    ],
+    [
+        "brand" => "Nissan",
+        "model" => "Qashqai",
+        "year" => "2022",
+        "description" => "1.3 DIG-T N-Connecta 5dr DCT",
+        "run" => "20,000 miles",
+        "fueltype" => "Petrol",
+        "gearbox" => "Automatic",
+        "price" => "23,500",
+        "currency" => "$",
+        "image" => "https://tse1.mm.bing.net/th/id/OIP.9cpeh0KR-UTEZ4T6Op-4OwHaEc?rs=1&pid=ImgDetMain&o=7&rm=3",
+    ],
+    [
+        "brand" => "Mazda",
+        "model" => "CX-5",
+        "year" => "2020",
+        "description" => "2.0 SKYACTIV-G SE-L Nav 5dr",
+        "run" => "35,000 miles",
+        "fueltype" => "Petrol",
+        "gearbox" => "Manual",
+        "price" => "24,000",
+        "currency" => "$",
+        "image" => "https://tse4.mm.bing.net/th/id/OIP.SUj4pBCjh9eIeCPdCI84wgHaEO?rs=1&pid=ImgDetMain&o=7&rm=3",
+    ],
+    [
+        "brand" => "Hyundai",
+        "model" => "Tucson",
+        "year" => "2021",
+        "description" => "1.6 T-GDi Premium 5dr DCT",
+        "run" => "25,000 miles",
+        "fueltype" => "Petrol",
+        "gearbox" => "Automatic",
+        "price" => "22,500",
+        "currency" => "$",
+        "image" => "https://th.bing.com/th/id/R.0890809370bb1e323481387f0dd9b697?rik=00jeZ8H94WtucA&pid=ImgRaw&r=0",
+    ]
+
+];
+
+
+
+$exboxcars = [
+    [
+        "brand" => "Volvo",
+        "model" => "FH16",
+    ],
+    [
+        "brand" => "Scania",
+        "model" => "R500",
+    ],
+    [
+        "brand" => "MAN",
+        "model" => "TGX",
+    ],
+    [
+        "brand" => "DAF",
+        "model" => "XF",
+    ],
+    [
+        "brand" => "Iveco",
+        "model" => "Stralis",
+    ],
+    [
+        "brand" => "Mercedes-Benz",
+        "model" => "Actros",
+    ],
+    [
+        "brand" => "Renault",
+        "model" => "T High",
+    ],
+    [
+        "brand" => "Kenworth",
+        "model" => "W900",
+    ],
+    [
+        "brand" => "Peterbilt",
+        "model" => "579",
+    ],
+    [
+        "brand" => "Freightliner",
+        "model" => "Cascadia",
+    ],
+    [
+        "brand" => "Mack",
+        "model" => "Anthem",
+    ],
+    [
+        "brand" => "Western Star",
+        "model" => "4900"
+    ]
+];
---
+++ variable.php
@@ -0,0 +1,4 @@
+<?php
+$brandname = "Labib-enterprise";
+
+?>
